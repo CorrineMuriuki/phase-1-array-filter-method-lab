c72c2fc407257de24f718e45c11babc2160ff5b1
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-08-31 23:29:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, attribute) {
+ return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, string){
+ return drivers.filter(function (driver) {return driver.toLowerCase().slice(0,string.length) === string.toLowerCase()})
+  }
+
+  function matchName(drivers, string){
+    return drivers.filter(function(driver) {return driver.name.toLowerCase() === string.toLowerCase()})
+  }
